ff449087b6d82b3655222ddf3ec4b13a2b724300
Jihyeon Kwon <kjh2868@gmail.com>
2022-02-04 18:48:31 +0900

    삭제

--- class_backend/index.ts
+++ class_backend/index.ts
@@ -5,10 +5,6 @@ console.log("Hello TypeScript!!");
 createConnection({
   type: "postgres",
   database: "postgres",
-  username: "postgres",
-  password: "postgres2021",
-  port: 5014,
-  host: "34.64.187.209",
   entities: ["./*.postgres.ts"], //어떤 table이 들어갈지("":현재모든파일)
   logging: true, //동기화 되었는지 관련 로그 기록
   synchronize: true, //DB와 Board(entities안의 javascript로만든 파일) 동기화
