0bc3d5bd77f0c30e5a328475ec39ed404e618fad
Jihyeon Kwon <kjh2868@gmail.com>
2022-02-04 15:49:40 +0900

    algorithm

--- programmers/day_15/day15_01.js
+++ programmers/day_15/day15_01.js
@@ -1,9 +1,19 @@
 function solution(a, b) {
-  var answer = 0;
+  let answer = 0;
   // return answer;
 
   for (let i = 0; i < a.length; i++) {
     answer += a[i] * b[i];
   }
+  return answer;
+}
+
+//연산 메서드 .reduce() 이용 가능
+function solution(a, b) {
+  let answer = a.reduce((acc, cur, i) => {
+    // console.log(acc,cur,b[i])
+    return acc + cur * b[i];
+  }, 0);
+
   return answer;
 }
--- programmers/day_15/day15_02.js
+++ programmers/day_15/day15_02.js
@@ -7,3 +7,24 @@ function solution(arr) {
     return (answer = arr.filter((el) => el > Math.min(...arr)));
   }
 }
+
+//for, if 반복문 이용
+function solution(arr) {
+  let answer = [];
+
+  //1. 제일 작은 수를 찾는 과정
+  let min = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (min > arr[i]) {
+      min = arr[i];
+    }
+  }
+  //2.제일 작은 수를 제외한 나머지 값들을 새로운 배열에 저장
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== min) {
+      answer.push(arr[i]);
+    }
+  }
+
+  return answer.length === 0 ? [-1] : answer;
+}
