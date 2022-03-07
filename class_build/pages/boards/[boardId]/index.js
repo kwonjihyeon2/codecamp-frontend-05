20f9e9fdccd2918d7cbadf2a3784fcf6e159bc29
Jihyeon Kwon <kjh2868@gmail.com>
2022-03-07 13:06:41 +0900

    update ssr

--- class_build/pages/boards/[boardId]/index.js
+++ class_build/pages/boards/[boardId]/index.js
@@ -1,12 +1,53 @@
 import { useRouter } from "next/router";
+import { Head } from "next/head";
+import { gql, request } from "graphql-request";
 
-export default function BoardsDetailPage() {
+export default function BoardsDetailPage(props) {
   const router = useRouter();
 
   return (
     <div>
-      안녕하세요 게시글 상세 페이지입니다, 게시글 ID는 {router.query.boardId}
-      입니다.
+      <Head>
+        <meta property="og:title" content={props.myBoardData.title} />
+        <meta property="og:description" content={props.myBoardData.contents} />
+        <meta property="og:image" content={props.myBoardData.images[0]} />
+      </Head>
+      <div>
+        안녕하세요 게시글 상세 페이지입니다, 게시글 ID는 {router.query.boardId}
+        입니다.
+      </div>
     </div>
   );
 }
+
+const FETCH_BOARD = gql`
+  query fetchBoard($boardId: ID!) {
+    fetchBoard(boardId: $boardId) {
+      title
+      contents
+      images
+    }
+  }
+`;
+
+export const getServerSideProps = async (context) => {
+  // pages에서만 가능한 next.js제공 함수 : 렌더될 파일보다 먼저 실행, SSR파일은 여기서 데이터 요청해야함
+  // ** useRouter같은 브라우저 작동 기능을 활용할 수 없음 -> context
+
+  const result = await request(
+    "https://backend05.codebootcamp.co.kr/graphql",
+    FETCH_BOARD,
+    { boardId: context.query.boardId }
+  );
+  //return data -> props로 default 파일에 전달( = props : {} ) : 데이터가 만들어진 페이지를 렌더링하게 됨
+  return {
+    props: {
+      myBoardData: {
+        title: result.fetchBoard.title,
+        contents: result.fetchBoard.contents,
+        images: result.fetchBoard.images,
+        // ...result
+      },
+    },
+  };
+};
