5bd799398bb76d31f20b810389851b7d71787bb6
kwonjihyeon2 <kjh2868@gmail.com>
2022-03-09 20:45:27 +0900

    dynamic routing

--- sample_build/pages/boards/[boardId]/index.js
+++ sample_build/pages/boards/[boardId]/index.js
@@ -1,3 +1,12 @@
+import { useRouter } from "next/router";
+
 export default function BoardsDetailPage() {
-  return <div>안녕하세요 게시글 상세페이지입니다.</div>;
+  const router = useRouter();
+
+  return (
+    <div>
+      안녕하세요 게시글 상세페이지입니다. 게시글 ID는 {router.query.boardId}
+      입니다.
+    </div>
+  );
 }
