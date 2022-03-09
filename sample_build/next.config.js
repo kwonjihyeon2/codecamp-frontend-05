157afc02cb3a2a9bd557b743b224f28f5f6a277d
kwonjihyeon2 <kjh2868@gmail.com>
2022-03-10 01:15:49 +0900

    3/10 docker try

--- sample_build/next.config.js
+++ sample_build/next.config.js
@@ -2,7 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
+  generateBuildId: () => "example-deploy-5",
   // slash기준 폴더 안에 파일을 생성
+  exportPathMap: () => ({
+    //static site 주소 입력
+    "/": { page: "/" },
+    "/boards": { page: "/boards" },
+    "/404": { page: "/404" },
+  }),
 };
 
 module.exports = nextConfig;
