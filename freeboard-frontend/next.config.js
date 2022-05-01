3f83f639762a8015cce0f3093ce5653c37ddc211
kwonjihyeon2 <kjh2868@gmail.com>
2022-05-02 01:03:59 +0900

    ssr meta test

--- freeboard-frontend/next.config.js
+++ freeboard-frontend/next.config.js
@@ -1,6 +1,16 @@
 module.exports = {
   reactStrictMode: true,
   trailingSlash: true,
+  generateBuildId: () => "freeboard-deploy", //ssr, ssg 동일한 파일이라고 인식시킴
+  exportPathMap: () => ({
+    //정적으로(storage)에서 받아올 파일
+    "/": { page: "/" },
+    "/boards": { page: "/boards" },
+    "/404": { page: "/404" },
+    "/Freeboard-api": { page: "/Freeboard-api" },
+    "/market": { page: "/market" },
+    "/mypage": { page: "/mypage" },
+  }),
   typescript: {
     ignoreBuildErrors: true,
   },
